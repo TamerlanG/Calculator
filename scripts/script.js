af736503893fb2064a2e337f3d8b2af57692953f
TamerlanG <tamerlanalbehairy@gmail.com>
2019-09-07 14:38:29 +0600

    Add operator functions and general skeleton of the program

--- scripts/script.js
+++ scripts/script.js
@@ -9,11 +9,41 @@
  * values to other functions on what to do
  */
 function getButtonPressed() {
+  alert("shit is working");
   let buttonPressed = document.getElementsByTagName("button");
+  let outputText = document.getElementById("textArea");
 
   for (let i = 0; i < buttonPressed.length; i++) {
     buttonPressed[i].addEventListener("click", () => {
-      return buttonPressed[i].textContent;
+      outputText.value = buttonPressed[i].textContent;
     });
   }
 }
+
+/**
+ * Main function of the webapp, this starts everything else
+ */
+function launch() {
+  getButtonPressed();
+}
+
+function addNumbers(number1, number2) {
+  return number1 + number2;
+}
+function subtractNumbers(number1, number2) {
+  return number1 - number2;
+}
+function multiplyNumbers(number1, number2) {
+  return number1 * number2;
+}
+function divideNumbers(number1, number2) {
+  return number1 / number2;
+}
+
+/**
+ * Operator function takes an operator, and two numbers then
+ * calls the appropriate function
+ */
+function operate(operator, number1, number2) {}
+
+launch();
